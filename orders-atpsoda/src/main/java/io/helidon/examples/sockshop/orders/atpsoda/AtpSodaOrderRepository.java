f729590dbe3e7e1c105ba4fdd3ee882a864a4be3
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-10-30 21:32:50 +0530

    adding

--- orders-atpsoda/src/main/java/io/helidon/examples/sockshop/orders/atpsoda/AtpSodaOrderRepository.java
+++ orders-atpsoda/src/main/java/io/helidon/examples/sockshop/orders/atpsoda/AtpSodaOrderRepository.java
@@ -171,7 +171,7 @@ public class AtpSodaOrderRepository implements OrderRepository {
 
                     for(Object o: _itemsArray){
                         if ( o instanceof JSONObject ) {
-                            items.add(Item(o.get("itemId").toString(),Integer.valueOf(o.get("quantity")), Float.parseFloat(o.get("unitPrice")));
+                            items.add(Item.builder().itemId(o.get("itemId").toString()).quantity(Integer.valueOf(o.get("quantity"))).unitPrice(Float.parseFloat(o.get("unitPrice"))).build();
                         }
                     }
                     orders.items = items;
