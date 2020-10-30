6c924d3bb9f30c5334f8217f1ebe076beb8216b9
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-10-31 00:05:49 +0530

    adding

--- orders-atpsoda/src/main/java/io/helidon/examples/sockshop/orders/atpsoda/AtpSodaOrderRepository.java
+++ orders-atpsoda/src/main/java/io/helidon/examples/sockshop/orders/atpsoda/AtpSodaOrderRepository.java
@@ -270,11 +270,10 @@ public class AtpSodaOrderRepository implements OrderRepository {
                     obj = parser.parse(jsonObject.get("customer").toString());
                     JSONObject _customer = (JSONObject) obj;
                     System.out.println(jsonObject.get("customer").toString());
-                    System.out.println(_customer.get("id").toString());
                     System.out.println(_customer.get("firstName").toString());
                     System.out.println(_customer.get("lastName").toString());
                     System.out.println(_customer.get("_id").toString());
-                    orders.customer =  Customer.builder().id(_customer.get("id").toString()).firstName(_customer.get("firstName").toString()).lastName(_customer.get("lastName").toString()).email(_customer.get("email").toString()).build();
+                    orders.customer =  Customer.builder().id(_customer.get("_id").toString()).firstName(_customer.get("firstName").toString()).lastName(_customer.get("lastName").toString()).email(_customer.get("email").toString()).build();
 
                    // orders.address = jsonObject.get("address").toString();  // Convert to Address 
 
